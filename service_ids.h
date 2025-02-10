cd0efd3d3957b7098c4dd7724b005e514830ba1e
Robert Vollmer <dev@robv.de>
2025-02-10 22:06:52 +0000

    Add header file for service IDs

---
+++ service_ids.h
@@ -0,0 +1,15 @@
+#ifndef SERVICE_IDS_H
+#define SERVICE_IDS_H
+
+namespace xbot::service_ids {
+enum : uint16_t {
+  EMERGENCY = 1,
+  DIFF_DRIVE = 2,
+  MOWER = 3,
+  IMU = 4,
+  POWER = 5,
+  GPS = 6,
+};
+}
+
+#endif  // SERVICE_IDS_H
